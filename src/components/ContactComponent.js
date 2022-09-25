0517600dc680024b4669eb5f1f57a59af8dca03e
Md Suhail Arif <suhailarif14@gmail.com>
2022-09-25 22:35:27 +0530

    Update ContactComponent.js

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -189,7 +189,7 @@ class Contact extends Component {
                             <Row className="form-group">
                                 <Label htmlFor="message" md={2}>Your Feedback</Label>
                                 <Col md={10}>
-                                    <Control.textarea model=".meassage" id="message" name="message"
+                                    <Control.textarea model=".message" id="message" name="message"
                                         rows="12"
                                         className="form-control" />
                                 </Col>
